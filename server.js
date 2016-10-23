5726243eb690765139662e0144d602b16ecbf6d5
Vikas Kaler <vikas.kalerv@gmail.com>
2016-10-23 01:26:30 -0400

    Todo post api

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [
-	{
-		id: 1,
-		description: 'Meet sam for lunch',
-		completed: false
-	},
-	{	id: 2,
-		description: 'Go to market',
-		completed: false
-	},
-	{	id: 3,
-		description: 'Beat Sahil',
-		completed: true
-	}
-];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
 	res.send('TODO Api Root');
 });
@@ -45,6 +36,20 @@ app.get('/todos/:id', function(req, res){
 			});
 	}
 });
+
+//posting a new todo item  /todos/todo
+app.post('/todos', function(req, res){
+	var body = req.body;
+	var todo = {
+		id : todoNextId,
+		description: body.description,
+		completed: body.completed
+	};
+	todos.push(todo);
+	todoNextId++;
+	res.json(todos);
+});
+
 app.listen(PORT, function(){
 	console.log('Express Listening at ' + PORT + '!');
 });
