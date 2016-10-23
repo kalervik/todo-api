78de4d5f75912c8fe58cae21aec94666df88ca6b
Vikas Kaler <vikas.kalerv@gmail.com>
2016-10-23 16:07:36 -0400

    Convert get /todos to sequelize

--- server.js
+++ server.js
@@ -14,24 +14,23 @@ app.get('/', function (req, res) {
 });
 //getRequest all todos
 app.get('/todos', function (req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
-	//if has property
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	var query = req.query;
+	var where = {};
+	if(query.hasOwnProperty('completed') && query.completed === 'true'){
+		where.completed = true;
+	}else if(query.hasOwnProperty('completed') && query.completed === 'false'){
+		where.completed = false;
 	}
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function (todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like : '%' + query.q + '%'
+		}
 	}
-	res.json(filteredTodos);
+	db.todo.findAll({where : where}).then(function(todos){
+		res.json(todos);
+	},function(e){
+		res.status(400).json(e);
+	});
 });
 
 //get todos/:id
@@ -51,19 +50,6 @@ app.get('/todos/:id', function (req, res) {
 	},function(e){
 		res.status(404).json(e);
 	});
-//	var matchedTodo = _.findWhere(todos, {
-//		id: todoId
-//	});
-//	if (matchedTodo) {
-//		res.json(matchedTodo);
-//	} else {
-//		res.status(404).send({
-//			error: {
-//				status: 404,
-//				message: 'TODO with this id doesn\'t exist'
-//			}
-//		});
-//	}
 });
 
 //posting a new todo item  /todos/todo
