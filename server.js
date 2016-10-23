52024a48f3e7b46256ad666284f493a30e894cff
Vikas Kaler <vikas.kalerv@gmail.com>
2016-10-23 02:38:57 -0400

    Added PUT todo

--- server.js
+++ server.js
@@ -72,6 +72,46 @@ app.delete('/todos/:id', function(req, res){
 			});
 	}
 });
+//PUT /todo/:id
+app.put('/todos/:id', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id : todoId});
+	if(!matchedTodo){
+		return res.status(404).send(
+			{
+				error :{
+					status: 404,
+					message: 'TODO with this id doesn\'t exist'
+				}
+			});
+	}
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}else if(body.hasOwnProperty('completed')){
+		return res.status(404).send(
+			{
+				error :{
+					status: 404,
+					message: 'Incorrect data provided.'
+				}
+			});
+	}
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	}else if(body.hasOwnProperty('description')){
+		return res.status(404).send(
+			{
+				error :{
+					status: 404,
+					message: 'Incorrect data provided description.'
+				}
+			});
+	}
+	matchedTodo = _.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+})
 app.listen(PORT, function(){
 	console.log('Express Listening at ' + PORT + '!');
 });
