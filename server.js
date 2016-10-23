b115c0bc8affa7acc859ca06ff31ac360fc6bb4c
Vikas Kaler <vikas.kalerv@gmail.com>
2016-10-23 03:05:56 -0400

    Added completed filter

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ app.get('/', function(req, res){
 });
 //getRequest all todos
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	//if has property
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos =_.where(filteredTodos, {completed : true});
+	}else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos =_.where(filteredTodos, {completed : false});
+	}
+	res.json(filteredTodos);
 });
 
 //get todos/:id
